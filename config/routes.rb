dbde40ca2842cdad540034e3bc081ceb8f71cd2e
Paula Lantran <paula.lantran@gmail.com>
2024-12-11 14:57:32 +0100

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static#home"
+  root "games#new"
 
   resources :games, only: [:new, :create, :destroy] do
     collection do
